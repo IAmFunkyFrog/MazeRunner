0de60380d1214bf5baf12e611833719670b5cb48
IAmFunkyFrog <trefilov.stepan@gmail.com>
2022-07-21 20:58:17 +0300

    [Refactor] Delete redundant code

--- src/main/kotlin/com/mazerunner/model/layout/MazeLayouts.kt
+++ src/main/kotlin/com/mazerunner/model/layout/MazeLayouts.kt
@@ -46,19 +46,3 @@ fun <T> MazeLayout.setCurrentRoom(
 fun MazeLayout.setCurrentRoom(mazeRoom: MazeRoom, oldCurrentRoomNewState: MazeRoomState = MazeRoomState.VISITED) {
     this.setCurrentRoom(mazeRoom, mazeRoom.stateProperty.get().info, oldCurrentRoomNewState)
 }
-
-// This layout type guarantees that all MazeRooms are GridMazeRoom and also
-// if there is MazeDoor between 2 GridMazeRoom then distance between them is 1
-// FIXME test code
-fun generateGridMazeLayout(width: Int, height: Int): MazeLayout {
-    if (width < 0 || height < 0) return MazeLayoutImpl(emptyList(), emptyList(), MazeLayoutState.GENERATED)
-
-    val arrayList = ArrayList<GridMazeRoom>()
-    for (i in (0..width)) {
-        for (j in (0..height)) {
-            arrayList.add(GridMazeRoom(i, j))
-        }
-    }
-
-    return MazeLayoutImpl(arrayList, emptyList(), MazeLayoutState.GENERATED)
-}
